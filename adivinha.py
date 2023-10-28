64d2810f81751d46bf449f4f19f2af7d8509ac07
Letícia Pagliato <106637848+pagliato@users.noreply.github.com>
2023-10-28 12:25:50 -0300

    Update final adivinha.py
    Ajustes finais

--- adivinha.py
+++ adivinha.py
@@ -1,24 +1,29 @@
 from random import randint
-
 print('#### Iníciando Jogo ####')
-
 random = randint(0, 100)
-chute = 0
-chances = 10
+chute = 0;
+chances = 10;
 for _ in range (chances):
     chute = input('Chute um número entre 0 a 100: ')
     if chute.isnumeric():
         chute = int(chute)
         chances = chances - 1
         if chute == random:
-            print('Parabéns, você venceu! O número era {} e você a inda tinha {} chances.'.format(random, chances))
+            print('')
+            print('Parabéns, você acertou! O número era {} e você a inda tinha {} chances.'.format(random, chances))
+            break;
         else:
-          if chute > random:
-                print('Você errou!!! Dica: É um número menor.')
-          else:
+            print('')
+            if chute > random:
+                print('Você errou!!! Dica: É menor.')
+                print('Chances restantes: {} .'.format(chances))
+            else:
                 print('Você errou!!! Dica: É um número maior.')
+                print('Chances restantes: {} .'.format(chances))
+            print('')
         if chances == 0:
-            print('Suas chances acabaram, você perdeu!')
-    if chances == 0 or chute == random:
-      break;
+            print('')
+            print('Suas chances acabaram. O número correto era {}.'.format(random))
+            print('')
+            break;
 print('#### Fim do Jogo ####')
