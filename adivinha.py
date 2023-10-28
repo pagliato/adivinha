720c8b9ef9bf183a63482bd4731e3010b08592a2
Letícia Pagliato <106637848+pagliato@users.noreply.github.com>
2023-10-28 11:48:37 -0300

    Update reduzir linhas adivinha.py
    Algoritmo reescrito a fim de reduzir o número de linhas de código.

--- adivinha.py
+++ adivinha.py
@@ -3,30 +3,22 @@ from random import randint
 print('#### Iníciando Jogo ####')
 
 random = randint(0, 100)
-chute = 0;
-chances = 10;
+chute = 0
+chances = 10
 for _ in range (chances):
     chute = input('Chute um número entre 0 a 100: ')
     if chute.isnumeric():
         chute = int(chute)
         chances = chances - 1
         if chute == random:
-            print('')
             print('Parabéns, você venceu! O número era {} e você a inda tinha {} chances.'.format(random, chances))
-            print('')
-            break;
         else:
-            print('')
-            if chute > random:
+          if chute > random:
                 print('Você errou!!! Dica: É um número menor.')
-            else:
+          else:
                 print('Você errou!!! Dica: É um número maior.')
-            print('Você ainda possui {} chances.'.format(chances))
-            print('')
         if chances == 0:
-            print('')
             print('Suas chances acabaram, você perdeu!')
-            print('')
-            break;
-
+    if chances == 0 or chute == random:
+      break;
 print('#### Fim do Jogo ####')
